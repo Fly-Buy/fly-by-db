1a58c348ed7a09db757dd5a67ffa70d7a6e2e496
Christopher Hutchinson <cshutchinson@gmail.com>
2016-02-06 13:12:19 -0700

    airline data

---
+++ airlines.js
@@ -0,0 +1,76 @@
+var airlines = [
+  {
+    "Airline": "Alaska Airlines",
+    "ICAO": "ASA",
+    "Callsign": "ALASKA",
+    "Founded": 1932
+  },
+  {
+    "Airline": "Allegiant Air",
+    "ICAO": "AAY",
+    "Callsign": "ALLEGIANT",
+    "Founded": 1997
+  },
+  {
+    "Airline": "American Airlines",
+    "ICAO": "AAL",
+    "Callsign": "AMERICAN",
+    "Founded": 1934
+  },
+  {
+    "Airline": "Delta Air Lines",
+    "ICAO": "DAL",
+    "Callsign": "DELTA",
+    "Founded": 1924
+  },
+  {
+    "Airline": "Frontier Airlines",
+    "ICAO": "FFT",
+    "Callsign": "FRONTIER FLIGHT",
+    "Founded": 1994
+  },
+  {
+    "Airline": "Hawaiian Airlines",
+    "ICAO": "HAL",
+    "Callsign": "HAWAIIAN",
+    "Founded": 1929
+  },
+  {
+    "Airline": "JetBlue Airways",
+    "ICAO": "JBU",
+    "Callsign": "JETBLUE",
+    "Founded": 1998
+  },
+  {
+    "Airline": "Southwest Airlines",
+    "ICAO": "SWA",
+    "Callsign": "SOUTHWEST",
+    "Founded": 1967
+  },
+  {
+    "Airline": "Spirit Airlines",
+    "ICAO": "NKS",
+    "Callsign": "SPIRIT WINGS",
+    "Founded": 1980
+  },
+  {
+    "Airline": "Sun Country Airlines",
+    "ICAO": "SCX",
+    "Callsign": "Sun Country",
+    "Founded": 1982
+  },
+  {
+    "Airline": "United Airlines",
+    "ICAO": "UAL",
+    "Callsign": "UNITED",
+    "Founded": 1926
+  },
+  {
+    "Airline": "Virgin America",
+    "ICAO": "VRD",
+    "Callsign": "REDWOOD",
+    "Founded": 2004
+  }
+];
+
+module.exports = airlines;
