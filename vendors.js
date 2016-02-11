000d14bf128d5787c9be9d0113f0baa8ee1a4875
Alex Bennett <alexandergraybennett@gmail.com>
2016-02-11 14:11:23 -0700

    adding vendor data

---
+++ vendors.js
@@ -0,0 +1,19 @@
+var vendors = [
+  {
+    "name": "Expedia",
+  },
+  {
+    "name": "Kayak",
+  },
+  {
+    "name": "Travelocity",
+  },
+  {
+    "name": "Orbitz",
+  },
+  {
+    "name": "Trivago"
+  }
+]
+
+module.exports = vendors;
