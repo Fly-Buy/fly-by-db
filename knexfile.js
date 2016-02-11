d8b121f84558aea740cffaa14ed22d47bd53df89
Alex Bennett <alexandergraybennett@gmail.com>
2016-02-11 14:12:45 -0700

    adding knexfile

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,4 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://xzohfnlzxudhxr:ppzxBTyCO46jA2B5KCrfuT020g@ec2-107-21-218-93.compute-1.amazonaws.com:5432/d9qflmp2hvbc0j?ssl=true'
   }
-
 };
